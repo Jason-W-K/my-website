aeab4cd9f0037bcb30c90056aab1f9801bbef8ae
Jason-W-K <jaybashee139@gmail.com>
2025-09-29 16:24:01 +0300

    Added images to all pages

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// Toggle mobile nav
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+navToggle.addEventListener('click', () => {
+  navLinks.classList.toggle('open');
+});
